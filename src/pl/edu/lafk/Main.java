6db8736e4aedd4094ec62278e0dbbbe3136d2c02
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2018-05-22 23:22:32 +0200

    Initial launcher idea Uses reflection to get all valid methods from F1 class. Incomplete.
    TODO:
    
    1. Works only on String params, while I need also Integer ones.
    2. Randomize lists that I call methods with.
    3. Gather all classes and ran them.
    4. Annotations may come in handy later.
    
    Saying that Java is nice because it works on all OS's is like saying that anal sex is nice because it works on all genders
        -- Alanna, on IRC

---
+++ src/pl/edu/lafk/Main.java
@@ -0,0 +1,44 @@
+package pl.edu.lafk;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Main main = new Main();
+        Stream<Method> methodStream = Arrays.stream(Functional1.class.getDeclaredMethods()).filter(m -> m.getModifiers() == 1);
+        Arrays.stream(Functional1.class.getDeclaredMethods()).filter(m -> m.getModifiers() == 1)
+                .filter(m -> m.getGenericParameterTypes()[0].getTypeName().equalsIgnoreCase("java.util.List<java.lang.String>"))
+                .forEach(m -> System.out.println(String.format("%s: %s", m.getName(), invoke(m, new Functional1(), main.strings()))));
+
+    }
+
+    private static List<String> invoke(Method m, Functional1 c, List<String> param) {
+        try {
+            return (List<String>) m.invoke(c, param);
+        } catch (IllegalAccessException|InvocationTargetException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    List<Integer> ints() {
+        List<Integer> ints = IntStream.of(1, 22, 103, 16, 8, 886, 8, 1, 10, 0).boxed().collect(Collectors.toList());
+        return ints;
+    }
+    List<String> strings() {
+        List<String> strings = new ArrayList();
+        strings.add("x");
+        strings.add("xax");
+        strings.add("xbxb");
+        strings.add("abc def");
+        return strings;
+    }
+}
