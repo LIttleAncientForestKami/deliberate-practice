bb80c79dd0bbacd91e2734f2c29db3119f528a04
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2022-06-19 20:03:18 +0200

    Feat: Python workout 1/50, a version with number of tries, if users wants to have it.
    C programmers never die. They are just cast into void.

--- book_python_workout/imperative_guess_the_number.py
+++ book_python_workout/imperative_guess_the_number.py
@@ -5,20 +5,30 @@ if __name__ == "__main__":
     low = 0
     high = 100
     print(f"Hello! I will ask you for a number from a range between {low} and {high} inclusive. Guessing game continues until you get it right!")
+    max_nr_of_tries = None
+    nr_of_tries = 0
+    try:
+        max_nr_of_tries = int(input("Input number of tries you want, or leave the input empty for unlimited (weird inputs will also mean unlimited tries): "))
+        if max_nr_of_tries <= 0: raise ValueError
+    except ValueError: max_nr_of_tries = None
     import random as r
     secret = r.randint(0,100)
     users = -1
     range_open = "<"
     range_close = ">"
-    while users != secret:
+    while users != secret and nr_of_tries != max_nr_of_tries:
         try:
-            msg = f"Range is {range_open}{low}, {high}{range_close}. Your number? "
+            msg = f"Range is {range_open}{low}, {high}{range_close}. Your number? (Attempt {nr_of_tries+1}) "
+            nr_of_tries += 1
             users = int(input(msg))
         except ValueError: 
             print("Numbers please. Numbers only. Or Ctrl+c to quit.")
             continue
         if users > secret: print("Too high!"); high = users; range_close = ")"
         if users < secret: print("Too low!"); low = users; range_open = "("
-    print("Well done, it was indeed %d, thanks for playing!" % secret)
+    if users != secret: 
+        print("Ouch. A shame, but you were only allowed %d tries in total." % max_nr_of_tries)
+    else: 
+        print("Well done, it was indeed %d, took you %d out of %d tries! Thanks for playing!" % (secret, nr_of_tries, max_nr_of_tries))
 
 
